8b1da025bc16d6649479a3710679b6c8d0757fdb
黄健 <huangjian@huangjiandeMacBook-Pro.local>
2019-09-08 11:28:01 +0800

    [中文]

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import zh_CN from 'antd/lib/locale-provider/zh_CN'
+import { LocaleProvider } from 'antd'
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+moment.locale('zh-cn');
 ReactDOM.render(
   <Router basename="/">
+    <LocaleProvider locale={zh_CN}>
     <App />
+    </LocaleProvider>
   </Router>,
   document.getElementById('root')
 )
